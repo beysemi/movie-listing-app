31481dda8646508c8d10e029071a7195b73e844b
Beysemi Emirhan BAYRAK <beysemiemirhanbayrak@gmail.com>
2022-11-19 01:16:36 +0300

    add meta-tag

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -29,6 +29,8 @@ const Head = ({ title, description, image, type }) => {
       />
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet"></link>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap-grid.css"></link>
     </NextHead>
